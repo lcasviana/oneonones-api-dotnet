e6a18e3a73c1a9a09c0f27888d39016ff09cb8e8
lcasviana <lcasviana@gmail.com>
2021-03-22 19:55:45 -0300

    Ajustando ordem das propriedades

--- Oneonones/Oneonones.Domain/Entities/EmployeeInputEntity.cs
+++ Oneonones/Oneonones.Domain/Entities/EmployeeInputEntity.cs
@@ -2,7 +2,7 @@ namespace Oneonones.Domain.Entities
 {
     public class EmployeeInputEntity
     {
-        public string Name { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
     }
 }
--- Oneonones/Oneonones.Infrastructure/ViewModels/EmployeeInputViewModel.cs
+++ Oneonones/Oneonones.Infrastructure/ViewModels/EmployeeInputViewModel.cs
@@ -2,7 +2,7 @@ namespace Oneonones.Infrastructure.ViewModels
 {
     public class EmployeeInputViewModel
     {
-        public string Name { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
     }
 }
