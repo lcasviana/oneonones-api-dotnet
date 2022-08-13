0ef413b647ae5aae7e0a53a0f952a277e306dd15
Lucas Viana <lcasviana@gmail.com>
2022-08-13 16:28:03 +0100

    Checkpoint

--- src/Oneonones/Program.cs
+++ src/Oneonones/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers(options =>
     options.SuppressAsyncSuffixInActionNames = false;
 });
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowCredentials().AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
 builder.Services.AddSwashbuckle();
 builder.Services.AddValidators();
 builder.Services.AddDatabase(builder.Configuration);
